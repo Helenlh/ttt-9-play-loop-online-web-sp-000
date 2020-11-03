24a4544bf0af2894701d154302fa768294bf022f
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-03 01:47:45 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,13 +38,10 @@ end
 # Define your play method below
 def play(board)
   counter = 0 
-  
+  until counter = 9 
   turn(board)
     counter +=1 
-  
- 
-    
-  puts "game over"
   end 
+   puts "game over"
 end 
 
